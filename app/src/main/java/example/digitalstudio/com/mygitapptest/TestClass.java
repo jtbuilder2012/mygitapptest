57d72b6f77d8de78d1234f9d85e031d716e96956
gittestuser <mygit123@using.com>
2016-05-18 22:06:54 +0100

    Add a new TestClass to test applying changes to the repo...

---
+++ app/src/main/java/example/digitalstudio/com/mygitapptest/TestClass.java
@@ -0,0 +1,56 @@
+package example.digitalstudio.com.mygitapptest;
+
+/**
+ * Created by Omar on 15/05/2016.
+ *
+ * These are test classes to experiment with the use of GitHub as the VCS for the project.
+ * A number of use cases will be tested to ensure no data loss...
+ *
+ * A number of difficult scenarios will be created to build a thorough understanding of the VCS.
+ *  - plugins - use of plugins to extend the integration of AS with GitHub
+ *  - .ignore file - ensuring the file is optimised for the project
+ *  - Add changes / commits
+ *      - project and file level
+ *  - Rewind commits
+ *  - Manage conflicts - add files from two different instances of AS (Android Studio)
+ *  - Merge changes between the local and remote repository
+ *  - Pull and Push changes from the remote / local repositories
+ *  - Diff - two objects are different states; merge or mange the difference
+ *         - comparing and resolving differences
+ *  - Revert - rollback changes
+ *  - Head - moving up / down the MASTER branch; dealing with situations of being ahead / behind on
+ *           the MAster branch
+ *  - Stashing  - how to use stashing
+ *  - Labels how to use Labels
+ *  - Branching
+ *      - add branches (locally)  - push the branch to GitHub to be merged with the MASTER
+ *      - merge with MASTER remote
+ *      - add branches locally and remotely
+ *      - life cycle branching
+ *      - branching to accommodate:
+ *          - lifecycle - sprints, unit tests,  integration test, acceptance test, performance test
+ *                      -  PROD release, maintenance release
+ *                      -  flavours - Free and Paid to accommodate variance in the code base
+ *                      - Good practice for managing the aforementioned stages of the development
+ *   - Need to ensure the project under version control on GitHub can be cloned and rebuilt
+ *     on another version of Android Studio
+ *     - Review - accept /reject code in the repository
+ *   - Synchronising the local and remote repository
+ *   - Patching - good practice for patching
+ *   - Rebasing - to help address conflicts - create some use cases and resolve
+ *   - Defect Management - logging and tracking issues on GutHub
+ *   - Cloning - a repository, making changes to certain elements and pushing only those changes
+ *               back to the repository for inclusion in the Master
+ *   - Good practice management of the repo to avoid difficulties in versioning the artifacts
+ *   - Checking and checking out of version control
+ *
+ *
+ */
+public class TestClass {
+
+    // Constructor
+    public TestClass() {
+
+
+    }
+}
